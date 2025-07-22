b47a07c7b8f0b4647b33cb6831d3140d62c57a6a
AkkachaiCE <akachai1624@gmail.com>
2025-07-22 17:51:28 +0700

    Refactor function into separate files

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() {
-			cleanText := cleanInput(scanner.Text())
-			fmt.Print("Your command was: " + cleanText[0])
-		}
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		}
-
-	}
-}
-
-func cleanInput(text string) []string {
-	words := strings.Fields(strings.TrimSpace(strings.ToLower((text))))
-	return words
+	startRepl()
 }
